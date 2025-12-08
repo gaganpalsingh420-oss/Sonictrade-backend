6fb51d62b76dfb622cda085ec1793fbefd212378
gaganpalsingh420-oss <gaganpalsingh420@gmail.com>
2025-12-08 16:51:29 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const stocksRoute = require("./routes/stocks");
+const portfolioRoute = require("./routes/portfolio");
+const mutualFundsRoute = require("./routes/mutualfunds");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/stocks", stocksRoute);
+app.use("/api/portfolio", portfolioRoute);
+app.use("/api/mutualfunds", mutualFundsRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Backend running on port", PORT);
+});
