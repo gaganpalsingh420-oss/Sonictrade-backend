68db243b3316ff054407aa225af7c7bb2592b5b3
gaganpalsingh420-oss <gaganpalsingh420@gmail.com>
2025-12-10 15:48:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const stocksRoute = require("./routes/stocks");
 const portfolioRoute = require("./routes/portfolio");
 const mutualFundsRoute = require("./routes/mutualfunds");
 
+const { startScheduler } = require("./services/stockFetcher");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -17,4 +19,6 @@ app.use("/api/mutualfunds", mutualFundsRoute);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Backend running on port", PORT);
+  // start the background polling
+  startScheduler();
 });
